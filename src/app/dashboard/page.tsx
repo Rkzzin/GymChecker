5203bc9fbdf0b5d83f83641125bb975aadeacfe9
Rick <henriquels5@al.insper.edu.br>
2024-07-16 10:33:03 -0300

    Setup páginas iniciais

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 export default function Dashboard() {
     return (
-      <div className="min-h-screen bg-gray-100">
+      <div className="min-h-screen bg-gray-100 text-gray-900">
         <header className="bg-white shadow">
           <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold leading-tight text-gray-900">Dashboard</h1>
--- src/app/subscriptions/page.tsx
+++ src/app/subscriptions/page.tsx
@@ -1,6 +1,6 @@
 export default function subscriptions() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-100 text-gray-900">
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold leading-tight text-gray-900">Matrículas</h1>
@@ -12,7 +12,7 @@ export default function subscriptions() {
           </nav>
         </div>
       </header>
-      
+
       <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
         <div className="px-4 py-6 sm:px-0">
           <h2 className="text-xl font-semibold">Overview</h2>
